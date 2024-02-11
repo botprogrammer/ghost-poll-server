ff6111480f56548e773a5165e456fdcfe0e95675
botprogrammer <p03goswami@gmail.com>
2024-02-11 13:10:00 +0530

    Server index and basic config

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./config/db";
+import { pollRoutes } from "./routes/poll.routes";
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+connectDB(); // Connect to MongoDB when the server starts
+
+app.use("/api/polls", pollRoutes);
+
+// Routes
+app.get("/", (_, res) => {
+  res.json({
+    message: "✅ Ghost Poll Server is running. ^_^",
+  });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
